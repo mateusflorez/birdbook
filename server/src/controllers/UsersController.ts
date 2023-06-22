325c3a548027a58ac2aa36dd304ee062db372cc0
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-06-22 12:33:11 -0300

    login with jsonwebtoken

--- server/src/controllers/UsersController.ts
+++ server/src/controllers/UsersController.ts
@@ -2,11 +2,22 @@ import { Request, Response, NextFunction } from "express"
 import Joi from "joi"
 import bcrypt from "bcrypt"
 import UsersService from "../services/UsersService"
+import { sign } from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const jsonSecretKey: string = process.env.JSON_SECRET ?? ""
 
 const registerUserScheme = Joi.object({
     username: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required()
+})
+
+const loginUserSchema = Joi.object({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
 })
 
 async function register(req: Request, res: Response, next: NextFunction) {
@@ -45,4 +56,45 @@ async function register(req: Request, res: Response, next: NextFunction) {
     }
 }
 
-export default { register }
+async function login(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { username, password } = req.body
+
+        const { error } = loginUserSchema.validate(req.body);
+
+        if (error) {
+            return res.json({
+                status: false,
+                message: "error." + error.details[0].context?.label
+            });
+        }
+
+        const user = await UsersService.getUser({ username: username })
+
+        if (!user)
+            return res.json({ message: "validation.incorrect", status: false })
+
+        const passwordCheck = await bcrypt.compare(password, user.password)
+        if (!passwordCheck)
+            return res.json({ message: "validation.incorrect", status: false })
+
+        if (jsonSecretKey == "")
+            throw new Error("couldn't generate access token")
+
+        const accessToken = sign({
+            id: user.id,
+            email: user.email
+        }, jsonSecretKey, {
+            expiresIn: 86400
+        })
+
+        return res.status(200).json({
+            status: true,
+            accessToken
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+export default { register, login }
--- server/src/routes/UserRoutes.ts
+++ server/src/routes/UserRoutes.ts
@@ -4,3 +4,6 @@ import UsersController from "../controllers/UsersController"
 const userRoutes = Router()
 
 userRoutes.post("/user", UsersController.register)
+userRoutes.post("/login", UsersController.login)
+
+export { userRoutes }
